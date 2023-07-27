cd81093d655a7ecabb2c552122fe68c44d713c66
Kai Weluda <kaiweluda@gmail.com>
2023-07-27 15:53:50 +0200

    fix: adds correct paths for bootstrap

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   root: "src",
+  resolve: {
+    alias: {
+      "~bootstrap": "node_modules/bootstrap",
+    },
+  },
   server: {
     port: 8080,
     hot: true,
