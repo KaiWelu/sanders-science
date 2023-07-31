855590aac1985da4c08e4d03bbd5e01eba2028a9
Kai Weluda <kaiweluda@gmail.com>
2023-07-31 16:09:52 +0200

    feat: adds about component and first section

---
+++ src/components/About.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <div class="container-fluid">
+      <h1 class="text-center">ABOUT OUR LAB</h1>
+      <p class="lead text-center">Key features of our laboratory</p>
+      <p>
+        During normal development and ageing the cells in our body can acquire
+        somatic mutations through diverse genome instability processes. These
+        mutations are propagated and selected for over time. As consequence, we
+        are actually a mosaic of unique somatic cell genomes, where any given
+        cell in a tissue may be genetically distinct from another.
+      </p>
+      <p>
+        Understanding how somatic mosaicism develops and evolves are central
+        questions in the Sanders laboratory. With a focus on the genome, we
+        develop and apply innovative single cell methods that explore the
+        mechanisms of genome instability to understand how mutations arise and
+        change cellular phenotypes in normal human tissues and disease states.
+      </p>
+      <p>
+        By integrating multi-omic experimental and computational readouts, we
+        ask how individual cells differ in terms of their unique genomic
+        profiles, epigenetic programs and transcriptional outputs, and directly
+        test the functional outcomes of somatic mutations to understand how they
+        contribute to tissue homeostasis and human disease.
+      </p>
+    </div>
+    <div class="container-fluid"></div>
+  </section>
+</template>
+<style scoped>
+/* h1 {
+  text-align: center;
+} */
+</style>
