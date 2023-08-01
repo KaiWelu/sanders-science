ae2b0f575662a2515c63fe964e070fb8e9fe4465
Kai Weluda <kaiweluda@gmail.com>
2023-08-01 15:00:38 +0200

    feat: adds partners section layout and additional global styling

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@
 import About from "./components/About.vue";
 import NavBar from "./components/NavBar.vue";
 import TeamView from "./components/TeamView.vue";
+import Partners from "./components/Partners.vue";
 </script>
 
 <template>
   <NavBar></NavBar>
   <TeamView></TeamView>
   <About></About>
+  <Partners></Partners>
 </template>
