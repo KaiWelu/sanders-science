0bcddb499f74400c504207960231f82e210333dd
Kai Weluda <kaiweluda@gmail.com>
2023-08-01 15:36:44 +0200

    feat: adds contact component

---
+++ src/components/Contact.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <div class="container-md">
+      <div class="headline-wrapper">
+        <h1 class="text-center">Contact</h1>
+      </div>
+      <div class="textblock-wrapper">
+        <div class="contact-card">
+          <font-awesome-icon
+            icon="fa-solid fa-location-dot"
+            class="contact-icon"
+            style="color: #1b1b8d"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.contact-icon {
+  height: 2rem;
+}
+</style>
--- src/js/main.js
+++ src/js/main.js
@@ -16,10 +16,15 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 /* import specific icons */
-import { faUserSecret, faDna } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUserSecret,
+  faDna,
+  faLocationDot,
+} from "@fortawesome/free-solid-svg-icons";
 
 /* add icons to the library */
 library.add(faUserSecret);
 library.add(faDna);
+library.add(faLocationDot);
 
 createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
