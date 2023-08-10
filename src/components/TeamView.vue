3eba76b3292f320b4da00651b2da5378f7fcc0d6
KaiWelu <kaiweluda@gmail.com>
2023-08-10 15:38:53 +0200

    feat: adds more styling and icon layout

--- src/components/TeamView.vue
+++ src/components/TeamView.vue
@@ -3,7 +3,6 @@ import { ref } from "vue";
 let showComicImg = ref(true);
 function toggleImg() {
   this.showComicImg = !this.showComicImg;
-  console.log("Hallo");
 }
 </script>
 
@@ -41,20 +40,12 @@ function toggleImg() {
                       alt="Ashley Sanders"
                     />
                   </div>
-                  <div class="col-auto">
+                  <div class="col-auto pt-2">
                     <h3 class="mb-0">Dr. Ashley Sanders</h3>
                     <p class="text-body-secondary mb-0">
                       Principal Investigator
                     </p>
                   </div>
-                  <!-- <div class="col-auto">
-                    <a href="#"
-                      ><font-awesome-icon
-                        icon="fa-solid fa-envelope"
-                        class="nav-icon"
-                    /></a>
-
-                  </div> -->
                 </div>
               </div>
             </button>
@@ -66,7 +57,9 @@ function toggleImg() {
             data-bs-parent="#accordion-team"
           >
             <div class="accordion-body">
-              <h3>Genomic Instability and Somatic Mosacisim</h3>
+              <h3 class="text-center mt-2 mb-3">
+                Genomic Instability and Somatic Mosacisim
+              </h3>
               <p>
                 Hi and welcome! I’m the Sanders Lab head and an independent
                 group leader at the Berlin Institute for Medical Systems Biology
@@ -82,7 +75,28 @@ function toggleImg() {
                 and pushing the edge of discovery. I also love yoga and
                 mountains. Happy research!
               </p>
-              <p></p>
+              <p class="text-center">
+                <a href="#" class="p-2"
+                  ><font-awesome-icon
+                    icon="fa-solid fa-envelope"
+                    class="team-contact-icon"
+                /></a>
+                <a href="#" class="p-2"
+                  ><font-awesome-icon
+                    icon="fa-solid fa-envelope"
+                    class="team-contact-icon"
+                /></a>
+                <a href="#" class="p-2"
+                  ><font-awesome-icon
+                    icon="fa-solid fa-envelope"
+                    class="team-contact-icon"
+                /></a>
+                <a href="#" class="p-2"
+                  ><font-awesome-icon
+                    icon="fa-solid fa-envelope"
+                    class="team-contact-icon"
+                /></a>
+              </p>
             </div>
           </div>
         </div>
@@ -150,144 +164,6 @@ function toggleImg() {
         </div>
       </div>
     </div>
-
-    <!-- <div class="accordion accordion-flush" id="accordionFlushExample">
-      <div class="accordion-item">
-        <h2 class="accordion-header">
-          <button
-            class="accordion-button collapsed"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#flush-collapseOne"
-            aria-expanded="false"
-            aria-controls="flush-collapseOne"
-          >
-            <div class="container text-center">
-              <img
-                class="comic-pic"
-                src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight2&accessoriesType=Prescription02&hairColor=Brown&facialHairType=Blank&clotheType=ShirtCrewNeck&clotheColor=Blue03&eyeType=WinkWacky&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Light"
-              />
-              <h3>Dr. Ashley Sanders</h3>
-              <p class="text-body-secondary">Principal Investigator</p>
-            </div>
-          </button>
-        </h2>
-        <div
-          id="flush-collapseOne"
-          class="accordion-collapse collapse"
-          data-bs-parent="#accordionFlushExample"
-        >
-          <div class="accordion-body">
-            <h4>Genomic Instability and Somatic Mosacisim</h4>
-            <p>
-              Hi and welcome! I’m the Sanders Lab head and an independent group
-              leader at the Berlin Institute for Medical Systems Biology (BIMSB)
-              at the MDC. We apply single cell genomic technologies to
-              biomedical and translational problems. My lab’s research broadly
-              aims to (1) develop new and innovative single cell technologies
-              that combine experimental and computational methods to explore
-              germline and somatic genetic variation with a new lens, and (2)
-              apply these methods to investigate how genetic mutations arise in
-              our cells and tissues, how they impact cell states and functions,
-              and how they contribute to human health and disease. I love
-              finding new applications, building strong collaborations and
-              pushing the edge of discovery. I also love yoga and mountains.
-              Happy research!
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="accordion-item">
-        <h2 class="accordion-header">
-          <button
-            class="accordion-button collapsed"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#flush-collapseOne"
-            aria-expanded="false"
-            aria-controls="flush-collapseOne"
-          >
-            <div class="container text-center">
-              <img
-                class="comic-pic"
-                src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight2&accessoriesType=Prescription02&hairColor=Brown&facialHairType=Blank&clotheType=ShirtCrewNeck&clotheColor=Blue03&eyeType=WinkWacky&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Light"
-              />
-              <h3>Dr. Ashley Sanders</h3>
-              <p class="text-body-secondary">Principal Investigator</p>
-            </div>
-          </button>
-        </h2>
-        <div
-          id="flush-collapseOne"
-          class="accordion-collapse collapse"
-          data-bs-parent="#accordionFlushExample"
-        >
-          <div class="accordion-body">
-            <h4>Genomic Instability and Somatic Mosacisim</h4>
-            <p>
-              Hi and welcome! I’m the Sanders Lab head and an independent group
-              leader at the Berlin Institute for Medical Systems Biology (BIMSB)
-              at the MDC. We apply single cell genomic technologies to
-              biomedical and translational problems. My lab’s research broadly
-              aims to (1) develop new and innovative single cell technologies
-              that combine experimental and computational methods to explore
-              germline and somatic genetic variation with a new lens, and (2)
-              apply these methods to investigate how genetic mutations arise in
-              our cells and tissues, how they impact cell states and functions,
-              and how they contribute to human health and disease. I love
-              finding new applications, building strong collaborations and
-              pushing the edge of discovery. I also love yoga and mountains.
-              Happy research!
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="accordion-item">
-        <h2 class="accordion-header">
-          <button
-            class="accordion-button collapsed"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#flush-collapseThree"
-            aria-expanded="false"
-            aria-controls="flush-collapseOne"
-          >
-            <div class="container text-center">
-              <img
-                class="comic-pic"
-                src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight2&accessoriesType=Prescription02&hairColor=Brown&facialHairType=Blank&clotheType=ShirtCrewNeck&clotheColor=Blue03&eyeType=WinkWacky&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Light"
-              />
-              <h3>Dr. Ashley Sanders</h3>
-              <p class="text-body-secondary">Principal Investigator</p>
-            </div>
-          </button>
-        </h2>
-        <div
-          id="flush-collapseThree"
-          class="accordion-collapse collapse"
-          data-bs-parent="#accordionFlushExample"
-        >
-          <div class="accordion-body">
-            <h4>Genomic Instability and Somatic Mosacisim</h4>
-            <p>
-              Hi and welcome! I’m the Sanders Lab head and an independent group
-              leader at the Berlin Institute for Medical Systems Biology (BIMSB)
-              at the MDC. We apply single cell genomic technologies to
-              biomedical and translational problems. My lab’s research broadly
-              aims to (1) develop new and innovative single cell technologies
-              that combine experimental and computational methods to explore
-              germline and somatic genetic variation with a new lens, and (2)
-              apply these methods to investigate how genetic mutations arise in
-              our cells and tissues, how they impact cell states and functions,
-              and how they contribute to human health and disease. I love
-              finding new applications, building strong collaborations and
-              pushing the edge of discovery. I also love yoga and mountains.
-              Happy research!
-            </p>
-          </div>
-        </div>
-      </div>
-    </div> -->
   </div>
 </template>
 
@@ -295,10 +171,23 @@ function toggleImg() {
 .accordion-item {
   border: none;
 }
+.icon-wrapper {
+  position: absolute;
+  top: 5.5rem;
+  left: 8rem;
+}
+
+.accordion-header {
+  position: relative;
+}
+.accordion-header:focus {
+  border: none;
+  background-color: hotpink;
+}
 
 .real-pic {
   border-radius: 50%;
-  height: 5rem;
+  height: 6rem;
 }
 
 .collapse-img {
@@ -309,5 +198,26 @@ function toggleImg() {
   height: 50%;
 }
 .accordion-body {
+  _background-color: #5bf0d7;
+}
+
+.accordion {
+  --bs-accordion-active-bg: #bcf6efa1;
+}
+
+.accordion-button:focus {
+  z-index: 3;
+  border-color: #fff !important;
+  outline: 0;
+  box-shadow: 0 0 0 0.25rem #fff !important;
+}
+.team-contact-icon {
+  height: 2rem;
+  padding: 0.2rem;
+  margin: 0.5rem;
+  color: #2588c5ed;
+}
+.team-contact-icon:hover {
+  color: #1b618d98;
 }
 </style>
